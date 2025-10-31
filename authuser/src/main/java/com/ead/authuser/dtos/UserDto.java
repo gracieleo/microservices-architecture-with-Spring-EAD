858cd2d99b67d33989e000f166eb07e502d21996
Graciele <oliveiragracie@gmail.com>
2025-10-31 20:25:58 -0300

    authuser api service

---
+++ authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -0,0 +1,60 @@
+package com.ead.authuser.dtos;
+
+import com.ead.authuser.validation.UsernameConstraint;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+
+    //fornece diferentes visualizações para o mesmo DTO. A validação é aplicada com base na operação (POST, PUT, etc.) no controller
+    public interface UserView {
+        public static interface RegistrationPost {}
+        public static interface UserPut {}
+        public static interface PasswordPut {}
+        public static interface ImagePut {}
+    }
+
+    @NotBlank(groups = UserView.RegistrationPost.class) 
+    @Size(min = 4, max = 50, groups = UserView.RegistrationPost.class)
+    @UsernameConstraint(groups = UserView.RegistrationPost.class)
+    @JsonView(UserView.RegistrationPost.class)
+    private String username;
+
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Email(groups = UserView.RegistrationPost.class)
+    @JsonView(UserView.RegistrationPost.class)
+    private String email;
+
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    @Size(min = 6, max = 20, groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    private String password;
+
+    @NotBlank(groups = UserView.PasswordPut.class)
+    @Size(min = 6, max = 20, groups = UserView.PasswordPut.class)
+    @JsonView({UserView.PasswordPut.class})
+    private String oldPassword;
+
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.UserPut.class})
+    @Size(max = 150, groups = {UserView.RegistrationPost.class, UserView.UserPut.class})
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
+    private String fullName;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
+    private String phoneNumber;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
+    private String cpf;
+
+    @NotBlank(groups = UserView.ImagePut.class)
+    @JsonView({UserView.ImagePut.class})
+    private String imageUrl;
+}
+
---
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -0,0 +1,53 @@
+package com.ead.authuser.models;
+
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL) //serialização de objeto Java para Json, ignora atributos nulos na visualização
+@Entity
+@Table(name = "TB_USERS")
+public class UserModel extends RepresentationModel<UserModel> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+    @Column(nullable = false, length = 255)
+    @JsonIgnore //ignora o atributo na serialização para Json
+    private String password;
+    @Column(nullable = false, length = 150)
+    private String fullName;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING) //salva como string no banco de dados
+    private UserStatus userStatus;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+    @Column(length = 20)
+    private String phoneNumber;
+    @Column(length = 20)
+    private String cpf;
+    @Column
+    private String imageUrl;
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime lastUpdateDate;
+}
